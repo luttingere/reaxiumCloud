c6fd88d45c1dabc945de78f1bf288edf34d10d60
vifg2184 <vladimir.fernandez21@gmail.com>
2016-06-02 14:11:46 -0430

    *Agregado  Servicio de tracking

---
+++ src/Model/Entity/RouteType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VladimirIlich
+ * Date: 31/5/2016
+ * Time: 10:21
+ */
+
+namespace App\Model\Entity;
+use Cake\ORM\Entity;
+
+class RouteType extends Entity{
+
+    protected $_accessible = [
+        '*' => true,
+    ];
+}
